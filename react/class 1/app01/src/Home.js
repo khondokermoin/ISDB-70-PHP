8c6172be3de38ac35e42c62a6f280d5496b87e08
khondokermoin <mouin2k19@gmail.com>
2026-06-06 12:05:40 +0600

    all environment settings

---
+++ react/class 1/app01/src/Home.js
@@ -0,0 +1,27 @@
+// rcc React Class Component
+// rfc	React Functional Component
+// rfce	React Functional Component + Export
+// rce	React Class Component + Export
+// rpc	React Pure Componen
+
+
+/* 
+-----------------------------------------
+Settings খুলুন (Ctrl + ,)
+emmet include languages সার্চ করুন
+settings.json-এ যোগ করুন:
+
+"emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  } 
+-------------------------------------------- 
+  */
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <p>Home</p>
+    
+  )
+}
