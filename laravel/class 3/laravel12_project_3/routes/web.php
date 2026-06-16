acbdbbf6a2ba874580a136ff61bdf4b49807d483
khondokermoin <mouin2k19@gmail.com>
2026-06-16 11:44:09 +0600

    laravel class 3

---
+++ laravel/class 3/laravel12_project_3/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/dashboard', function () {
+    return view('backend.dashboard');
+});
+Route::get('/students', function () {
+    return view('backend.students.index');
+});
+Route::get('/students/create', function () {
+    return view('backend.students.create');
+});
